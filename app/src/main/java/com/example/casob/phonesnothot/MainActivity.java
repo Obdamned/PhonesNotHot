bbff40374eb30b4ab65d4a0e54423e786e74076c
Cas Obdam <casobdam@gmail.com>
2017-12-17 19:56:41 +0100

    Updated UI and added ButterKnife

--- app/src/main/java/com/example/casob/phonesnothot/MainActivity.java
+++ app/src/main/java/com/example/casob/phonesnothot/MainActivity.java
@@ -2,12 +2,27 @@ package com.example.casob.phonesnothot;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+    @BindView(R.id.save_button) Button mAskButton;
+    @BindView(R.id.ask_button) Button mSaveButton;
+    @BindView(R.id.wondering_teen) ImageView mWonderingTeen;
+    @BindView(R.id.teen_question) TextView mQuestionTeen;
+    @BindView(R.id.big_shaq) ImageView mBigShaq;
+    @BindView(R.id.big_shaq_answer) TextView mShaqAnswer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        //yg6554r
     }
 }
