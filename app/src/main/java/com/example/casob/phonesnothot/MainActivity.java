6eed2b022d391bf4b4171289cd3045946e202491
Cas Obdam <casobdam@gmail.com>
2017-12-18 21:49:56 +0100

    Finished Big Shaq sounds and added sensormanager/listener

--- app/src/main/java/com/example/casob/phonesnothot/MainActivity.java
+++ app/src/main/java/com/example/casob/phonesnothot/MainActivity.java
@@ -1,5 +1,10 @@
 package com.example.casob.phonesnothot;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.media.MediaPlayer;
 import android.os.AsyncTask;
 import android.os.Bundle;
@@ -15,7 +20,7 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
     @BindView(R.id.save_button)
     Button mAskButton;
     @BindView(R.id.ask_button)
@@ -28,13 +33,43 @@ public class MainActivity extends AppCompatActivity {
     ImageView mBigShaq;
     @BindView(R.id.big_shaq_answer)
     TextView mShaqAnswer;
-    MediaPlayer ShaqSound = new MediaPlayer();
+
+    private MediaPlayer ShaqSound = new MediaPlayer();
+
+    private SensorManager mSensorManager;
+    private Sensor mPhoneTemperature;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+
+        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        mPhoneTemperature = mSensorManager.getDefaultSensor(Sensor.TYPE_AMBIENT_TEMPERATURE);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mSensorManager.registerListener(this, mPhoneTemperature,
+                SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mSensorManager.unregisterListener(this);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
     }
 
     @OnClick(R.id.ask_button)
@@ -43,6 +78,49 @@ public class MainActivity extends AppCompatActivity {
         mQuestionTeen.setVisibility(View.VISIBLE);
         WaitAsync waitSync = new WaitAsync();
         waitSync.execute();
+
+    }
+
+    public void askBigShaq() {
+        mBigShaq.setVisibility(View.VISIBLE);
+        mShaqAnswer.setVisibility(View.VISIBLE);
+        getRandomSound();
+        ShaqSound.start();
+    }
+
+    private void getRandomSound() {
+        Random random = new Random();
+        int randomInt = random.nextInt(9 - 1 + 1) + 1;
+
+        switch (randomInt) {
+            case 1:
+                ShaqSound = MediaPlayer.create(this, R.raw.mans_never_be_hot);
+                break;
+            case 2:
+                ShaqSound = MediaPlayer.create(this, R.raw.boom);
+                break;
+            case 3:
+                ShaqSound = MediaPlayer.create(this, R.raw.no_ketchup);
+                break;
+            case 4:
+                ShaqSound = MediaPlayer.create(this, R.raw.raw_sauce);
+                break;
+            case 5:
+                ShaqSound = MediaPlayer.create(this, R.raw.smoke_trees);
+                break;
+            case 6:
+                ShaqSound = MediaPlayer.create(this, R.raw.the_ting_goes_skraa);
+                break;
+            case 7:
+                ShaqSound = MediaPlayer.create(this, R.raw.two_plus_two);
+                break;
+            case 8:
+                ShaqSound = MediaPlayer.create(this, R.raw.yo);
+                break;
+            case 9:
+                ShaqSound = MediaPlayer.create(this, R.raw.you_done_now);
+                break;
+        }
     }
 
     @OnClick(R.id.save_button)
@@ -53,56 +131,12 @@ public class MainActivity extends AppCompatActivity {
         mShaqAnswer.setVisibility(View.GONE);
     }
 
-    public void askBigShaq() {
-        mBigShaq.setVisibility(View.VISIBLE);
-        mShaqAnswer.setVisibility(View.VISIBLE);
-        getRandomSound(ShaqSound);
-        ShaqSound.start();
-    }
-
-    private MediaPlayer getRandomSound(MediaPlayer shaqSound) {
-        Random random = new Random();
-        int randomInt = random.nextInt(10 - 1 + 1) + 1;
-        MediaPlayer ShaqSound = shaqSound;
-
-        switch (randomInt) {
-            case 1:
-                ShaqSound = MediaPlayer.create(this, R.raw.mans_never_be_hot);
-                break;
-            case 2:
-                ShaqSound = MediaPlayer.create(this, R.raw.mans_never_be_hot);
-                break;
-            case 3:
-                ShaqSound = MediaPlayer.create(this, R.raw.mans_never_be_hot);
-                break;
-            case 4:
-                ShaqSound = MediaPlayer.create(this, R.raw.mans_never_be_hot);
-                break;
-            case 5:
-                ShaqSound = MediaPlayer.create(this, R.raw.mans_never_be_hot);
-                break;
-            case 6:
-                ShaqSound = MediaPlayer.create(this, R.raw.mans_never_be_hot);
-                break;
-            case 7:
-                ShaqSound = MediaPlayer.create(this, R.raw.mans_never_be_hot);
-                break;
-            case 8:
-                ShaqSound = MediaPlayer.create(this, R.raw.mans_never_be_hot);
-                break;
-            case 9:
-                ShaqSound = MediaPlayer.create(this, R.raw.mans_never_be_hot);
-                break;
-        }
-        return ShaqSound;
-    }
-
     public class WaitAsync extends AsyncTask<Void, Void, Void> {
 
         @Override
         protected Void doInBackground(Void... voids) {
             try {
-                Thread.sleep(2000);
+                Thread.sleep(1500);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
